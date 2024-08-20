ef86cde7068ce98e51356fdb814141ba9657f5da
mr-jay <171690269+mr-jay-04@users.noreply.github.com>
2024-08-20 19:11:17 +0530

    Fix winCheck missing argument and conditional

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function playGame(){
             console.log("You win! Rock beats scissor");
             humanScore+=1;
         }
-        else if (humanChoice.toLowerCase()=="paper" && compChoice=="scissors"){
+        else if (humanChoice.toLowerCase()=="paper" && compChoice=="scissor"){
             console.log("You lose! Scissor beats paper");
             compScore+=1;
         }
@@ -54,7 +54,7 @@ function playGame(){
             console.log("You lose! Rock beats scissor");
             compScore+=1;
         }
-        else if (humanChoice.toLowerCase()=="paper" && compChoice=="rock"){
+        else if (humanChoice.toLowerCase()=="scissor" && compChoice=="paper"){
             console.log("You win! Paper beats rock");
             humanScore+=1;
         }
@@ -62,11 +62,16 @@ function playGame(){
 
     for (let i=0; i<5; i++){
         compChoice = getComputerChoice();
+        console.log(compChoice);
         humanChoice = getHumanChoice();
+        console.log(humanChoice);
 
         playRound(humanChoice,compChoice);
     }
-    checkWinner();
+
+    console.log(humanScore);
+    console.log(compScore);
+    checkWinner(humanScore,compScore);
 }
 
 playGame();
