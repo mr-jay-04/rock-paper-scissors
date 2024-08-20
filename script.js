5d3a2373095e8ddc8533a92be62b702eb9e50c17
mr-jay <171690269+mr-jay-04@users.noreply.github.com>
2024-08-20 18:47:30 +0530

    Add script for rock paper scissor

---
+++ script.js
@@ -0,0 +1,72 @@
+function getComputerChoice(){
+    let compChoice=Math.random();
+
+    if (compChoice<0.33){
+        return "rock";
+    }
+    else if (compChoice<0.66){
+        return "paper";
+    }
+    else {
+        return "scissor";
+    }
+}
+
+
+function getHumanChoice(){
+    let humanChoice=prompt("What's your choice? ");
+    return humanChoice;
+}
+
+function checkWinner(humanScore,compScore){
+    if (humanScore>compScore) console.log("Game Over! You win");
+    else if (humanScore==compScore) console.log("Game Over! It's a tie");
+    else console.log("Game over! Computer wins");
+}
+
+function playGame(){
+
+    let humanScore = 0;
+    let compScore = 0;
+
+    function playRound(humanChoice, compChoice){
+        if (humanChoice.toLowerCase()==compChoice){
+            console.log("Tie");
+        }
+    
+        else if (humanChoice.toLowerCase()=="rock" && compChoice=="paper"){
+            console.log("You lose! Paper beats rock");
+            compScore+=1;
+        }
+        else if (humanChoice.toLowerCase()=="rock" && compChoice=="scissor"){
+            console.log("You win! Rock beats scissor");
+            humanScore+=1;
+        }
+        else if (humanChoice.toLowerCase()=="paper" && compChoice=="scissors"){
+            console.log("You lose! Scissor beats paper");
+            compScore+=1;
+        }
+        else if (humanChoice.toLowerCase()=="paper" && compChoice=="rock"){
+            console.log("You win! Paper beats rock");
+            humanScore+=1;
+        }
+        else if (humanChoice.toLowerCase()=="scissor" && compChoice=="rock"){
+            console.log("You lose! Rock beats scissor");
+            compScore+=1;
+        }
+        else if (humanChoice.toLowerCase()=="paper" && compChoice=="rock"){
+            console.log("You win! Paper beats rock");
+            humanScore+=1;
+        }
+    }
+
+    for (let i=0; i<5; i++){
+        compChoice = getComputerChoice();
+        humanChoice = getHumanChoice();
+
+        playRound(humanChoice,compChoice);
+    }
+    checkWinner();
+}
+
+playGame();
